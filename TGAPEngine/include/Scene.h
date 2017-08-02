5803cafdd253cf7e858e6571c2010094d3e05c83
LiquidMitrogen <szymonbiernacki91@gmail.com>
2017-08-02 22:15:16 +0200

    refactoring

--- TGAPEngine/include/Scene.h
+++ TGAPEngine/include/Scene.h
@@ -6,15 +6,20 @@
 namespace engine{
 	class Scene
 	{
+	protected:
+		std::vector<Entity * > entities;
+
 	public:
+		//ctor
 		Scene();
+		//dtor
 		virtual ~Scene();
+		//methods
 		void addEntity(Entity *e);
 		void removeEntity(Entity *e);
 		void draw(DrawingContext * context);
 		Entity * findEntityByName(std::string name);
-	protected:
-		std::vector<Entity * > entities;
+	
 	};
 
 }
